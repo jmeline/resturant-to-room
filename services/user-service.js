c943035ae6508863afdd78e7d34d81bc7144fe41
Jacob Meline <jacob.meline@gmail.com>
2015-06-19 21:33:13 +0000

    Adding bcrypt password hashing to project

--- services/user-service.js
+++ services/user-service.js
@@ -1,24 +1,31 @@
+var bcrypt = require('bcrypt');
 var User = require('../models/user').User;
 
 exports.addUser = function(user, next) {
-  var newUser = new User({
-    firstName: user.firstName,
-    lastName: user.lastName,
-    roomNumber: user.roomNumber,
-    email: user.email.toLowerCase(),
-    password: user.password
-  });
-  
-  newUser.save(function(err) {
-    if (err) {
-      return next(err);
-    }
-    next(null);
-  });
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) {
+            return next(err);
+        }
+        var newUser = new User({
+            firstName: user.firstName,
+            lastName: user.lastName,
+            roomNumber: user.roomNumber,
+            email: user.email.toLowerCase(),
+            password: hash
+        });
+        newUser.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+            next(null);
+        });
+    });
 };
 
 exports.findUser = function(email, next) {
-  User.findOne({email: email.toLowerCase()}, function(err, user) {
-    next(err, user);    
-  });
+    User.findOne({
+        email: email.toLowerCase()
+    }, function(err, user) {
+        next(err, user);
+    });
 };
